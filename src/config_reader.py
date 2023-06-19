2a7d3496a26c6b7be77f7c31301fa6b0ec40b10f
Paul Kepley <pakepley@gmail.com>
2023-06-19 12:06:23 -0400

    some configuration options for the animator

--- src/config_reader.py
+++ src/config_reader.py
@@ -27,3 +27,36 @@ def get_db_config():
 
     return cfg
 
+
+def get_start_loc():
+    config = ConfigParser()
+    config.read(config_path)
+    loc_conf = config['location']
+
+    cfg = {}
+    cfg['start_lon'] =  float(loc_conf['start_lon'])
+    cfg['start_lat'] =  float(loc_conf['start_lat'])
+
+    return cfg
+
+
+def get_usual_route_file():
+    config = ConfigParser()
+    config.read(config_path)
+
+    if config.has_section('route') and \
+       config.has_option('route', 'route_file'):
+        return Path(config['route']['route_file'])
+
+    return None
+
+
+def get_curve_clusterer_path():
+    config = ConfigParser()
+    config.read(config_path)
+
+    if config.has_section('clusterer') and \
+       config.has_option('clusterer', 'clusterer_path'):
+        return Path(config['clusterer']['clusterer_path'])
+
+    return None
